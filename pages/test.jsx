ab436317c85d86eeae0c09c5ec92c6296d9db8f2
Ivanrizkys <71715895+Ivanrizkys@users.noreply.github.com>
2021-12-12 20:21:19 +0700

    Initial Commit

---
+++ pages/test.jsx
@@ -0,0 +1,71 @@
+import Head from "next/head";
+
+const Test = ({data}) => {
+	return (
+		<div className="flex flex-col">
+			<Head>
+				<link
+					rel="stylesheet"
+					href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+					integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
+					crossOrigin="anonymous"
+				/>
+			</Head>
+			<div className="font-body">
+				<div className="h-full bg-gray-100">
+					<div className="block box-border bg-white max-w-md w-full mx-auto h-full">
+						<div className="flex flex-col">
+							<div className="mx-5 my-10 h-screen">
+								{/* Mulai ngoding di sini */}
+								{/* Title */}
+								<div className="font-normal font-main text-gray-900 text-2xl">
+									Bantuan
+								</div>
+
+								<p className="text-sm font-secondary text-gray-500 mt-3">
+									Kamu butuh bantuan? Lihatlah beberapa daftar
+									pertanyaan berikut
+								</p>
+
+								<div className="pt-7"></div>
+								{data.map((faq) => (
+									<div
+										key={faq.id_faq}
+										className="collapse border-2 rounded-box border-blue-secondary collapse-arrow font-secondary w-full mt-4 py-0 text-gray-500"
+									>
+										<input type="checkbox" />
+										<div className="collapse-title">
+											{faq.pertanyaan}
+										</div>
+										<div className="collapse-content ">
+											<p className="text-sm">
+												{faq.jawaban}
+											</p>
+										</div>
+									</div>
+								))}
+								<div className="text-xs font-secondary text-center text-gray-500 mt-12">
+									© 2021—Future Nine Dots Labs
+								</div>
+							</div>
+							<div className="w-full h-64"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export async function getStaticProps() {
+	const getFaq = await fetch("https://petspace.vercel.app/api/admin/faq");
+	const resFaq = await getFaq.json();
+	return {
+		props: {
+            data: resFaq.data,
+        },
+        revalidate: 10
+	};
+}
+
+export default Test;
